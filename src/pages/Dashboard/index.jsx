e093120ffd9a051c74012b5a266979f54046eba5
Paulo Vaz <pvaz1006@gmail.com>
2023-08-16 17:24:25 -0300

    fix: remove button primary

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,4 +1,3 @@
-import { ButtonPrimary } from "../../components/Buttons";
 import Title from "../../components/Titles";
 import 'style.scss'
 const Dashboard = () => {
@@ -9,7 +8,6 @@ const Dashboard = () => {
     <div className="box-dashboard">
       <img src={userImage} />
       <Title>{userName}</Title>
-      {/* <ButtonPrimary onCLick={}>Logout</ButtonPrimary> */}
   </div>
   )
  
